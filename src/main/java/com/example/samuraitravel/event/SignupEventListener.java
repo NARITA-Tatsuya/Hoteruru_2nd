c36325e9169f292592e885192b5d70a54a4f0273
N-Tatsuya <136430977+N-Tatsuya@users.noreply.github.com>
2025-02-11 22:33:37 +0900

    4day

--- src/main/java/com/example/samuraitravel/event/SignupEventListener.java
+++ src/main/java/com/example/samuraitravel/event/SignupEventListener.java
@@ -25,12 +25,10 @@ public class SignupEventListener {
 		User user = signupEvent.getUser();
 		String token = UUID.randomUUID().toString();
 		verificationTokenService.create(user, token);
-
 		String recipientAddress = user.getEmail();
 		String subject = "メール認証";
 		String confirmationUrl = signupEvent.getRequestUrl() + "/verify?token=" + token;
 		String message = "以下のリンクをクリックして会員登録を完了してください。";
-
 		SimpleMailMessage mailMessage = new SimpleMailMessage();
 		mailMessage.setTo(recipientAddress);
 		mailMessage.setSubject(subject);
